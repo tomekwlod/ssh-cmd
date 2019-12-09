af74528291c75a6c9da6dd42050bce2b25ad7ed9
Tomasz Wlodarczyk <tomek.wlod@gmail.com>
2019-12-09 21:53:24 +0000

    Just errors handling

--- cmd/main.go
+++ cmd/main.go
@@ -239,7 +239,7 @@ func servers() (d RemoteEntities) {
 	// respond to the client with the error message and a 400 status code.
 	err = json.NewDecoder(res.Body).Decode(&d)
 	if err != nil {
-		log.Fatal("error trrt", err)
+		log.Fatalf("An error occured\n'%v'", err)
 	}
 
 	// rr, err := utils.PrettyJson(d)
